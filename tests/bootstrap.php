775845615deb40df83b687d20d0cd3e2c2e7811f
Patrick Forget <patforg@oboxmedia.com>
2014-04-05 14:49:37 -0400

    removed unecessary stuff from bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,4 @@
  * @author Patrick Forget <patforg at geekpad.ca>
  */
 
-
-
-$loader = require_once __DIR__ .'/../vendor/autoload.php';
-//$loader->addPsr0('Monolog\\', __DIR__.'/Clapp');
+require_once __DIR__ .'/../vendor/autoload.php';
